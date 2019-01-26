17331187dbcba65adf0a8ef5a2bf0c48674b828d
nopara73 <adam.ficsor73@gmail.com>
2019-01-26 18:46:11 +0100

    Add project files.

---
+++ WasabiEncryptedSecretTester/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WasabiEncryptedSecretTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
